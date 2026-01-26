0fef0fe1b6023bb9374de7e4a8e520f1cdfb19d6
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-01-26 15:45:50 +0600

    this is my firts solution on hackerrank

---
+++ Say_Hello,World!.cpp
@@ -0,0 +1,12 @@
+//https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+//problem name: Say_Hello,World.cpp
+
+//code:
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
